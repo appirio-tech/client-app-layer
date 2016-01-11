8bc990bd1d84f2a18d898b15ce78c6987363ccde
Andrew Selbie <aselbie@gmail.com>
2016-01-11 10:20:00 -0800

    Just deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,3 @@
 require('./node_modules/coffee-script/register');
 
-config = require('appirio-tech-webpack-config')({
-  dirname: __dirname,
-  entry: './example/index'
-});
-
 module.exports = config;
